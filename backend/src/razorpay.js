6e283e2403a6e7687c948cd398e05e1311c5fdee
Akshat Jain <156729933+AkshatJain05@users.noreply.github.com>
2025-08-17 12:31:47 +0530

    Update razorpay.js

--- backend/src/razorpay.js
+++ backend/src/razorpay.js
@@ -4,9 +4,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
+const razorpay = (req,res)=>{
+  console.log("payment")
+}
 
 export default razorpay;
